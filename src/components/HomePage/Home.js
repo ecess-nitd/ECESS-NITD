192c3b2575989e0b99185d83e987cd669ed0438b
Jayesh Toshniwal <jayeshvr1110@gmail.com>
2024-06-24 23:13:34 +0530

    Added Carousel

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -5,26 +5,33 @@ import ImageAccordion from "./carousel.js";
 import img1 from '../../images/ecedepartment1.jpeg';
 import img2 from '../../images/ecedepartment2.jpeg';
 import img3 from '../../images/ecedept1-square.png';
+import Carousel from "./carousel-actual.js";
 
 
 
 const items = [
-  { image: `${img1}`},
+  { image: `${img1}` },
   { image: `${img2}` },
   { image: `${img3}` },
 ];
 
 function Home() {
+
+  const images = [
+    `${img1}`,
+    `${img2}`,
+    `${img3}`,
+  ];
   return (
     <div className="whole-website">
       <div
         className="primary-section"
-        style={{ backgroundImage: `url()` }}
+        style={{ backgroundImage: `url(${bg})` }}
       ></div>
       <div className="secondary-section">
         <div className="section " >
           <div className="card one">
-            <div className="heading">MISSION</div>
+            <div className="heading">Mission</div>
             <div className="description mission-desc">
               <p> To develop a community by the students of the Electronics and
                 Communication Department that will work towards the welfare of the
@@ -42,9 +49,9 @@ function Home() {
 
         <div className="section ">
           <div className="card two">
-            <div className="heading">VISION</div>
-            <div className="description">
-              <p> To produce highly competent and resourceful young engineers
+            <div className="heading">Vision</div>
+            <div className="description ">
+              <p className="vision-desc"> To produce highly competent and resourceful young engineers
                 who can perform well in varied professions. To develop a
                 strong fundamental base which enables students to explore
                 academic and collaborative interactions with industry,
@@ -56,7 +63,7 @@ function Home() {
 
         <div className="section ">
           <div className="card three">
-            <div className="heading">VALUES</div>
+            <div className="heading">Values</div>
             <div className="description">
               <p> The embryonic formation of the Department of Electronics and
                 Communication Engineering was in the year 1983 with the
@@ -73,6 +80,7 @@ function Home() {
       </div>
       {/* <CarouselFadeExample /> */}
       <ImageAccordion items={items} />
+      <Carousel images={images} />
     </div>
   );
 }
